2beb29f4428c56fdaf8a658402201f9c2098fb82
DP <git@dakotahpena.dev>
2021-06-23 19:54:24 -0700

    Moved some functions to specific file

--- bookmarks.go
+++ bookmarks.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"net/http"
+	"fmt"
+	"strconv"
+	"log"
+)
+
 type Bookmark struct {
 	Id            int
 	Url           string
@@ -14,4 +21,67 @@ type Bookmarks struct {
 	Next     string
 	Previous string
 	Results  []Bookmark
+}
+
+/*
+ * Makes a request to the bookmark URL and checks HTTP status codes.
+ * If a 404 is found, it delegates archiving the link.
+ */
+ func testBookmark(bmark *Bookmark) {
+	var resp *http.Response
+	var err error
+	resp, err = http.Get(bmark.Url)
+	if err != nil {
+		print(err)
+	}
+
+	defer resp.Body.Close()
+
+	renderStatusLine(resp.StatusCode, bmark.Url)
+
+	if resp.StatusCode == http.StatusNotFound {
+		archiveBookmark(bmark)
+	}
+
+	if resp.StatusCode == http.StatusForbidden {
+		fmt.Println("Page forbidden access with " + bmark.Url)
+
+		// body, err := ioutil.ReadAll(resp.Body)
+		// if err != nil {
+		// 	print(err)
+		// }
+
+		// fmt.Print(string(body))
+		// fmt.Println("body")
+	}
+}
+
+/*
+ * Archives a bookmark.
+ */
+func archiveBookmark(bmark *Bookmark) {
+	fmt.Println("  " + bmark.Website_title)
+	fmt.Println("  " + bmark.Url)
+	fmt.Println("\n  Archiving this bookmark…")
+
+	url := baseUrl + "/api/bookmarks/" + strconv.Itoa(bmark.Id) + "/archive/"
+	bearer := "Token " + token
+
+	req, err := http.NewRequest("POST", url, nil)
+	req.Header.Add("Authorization", bearer)
+	if err != nil {
+		log.Println("Error: ", err)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("Error on response.\n[ERROR] -", err)
+	}
+
+	if resp.Body != nil {
+		defer resp.Body.Close()
+	}
+
+	fmt.Println("  … done!")
 }
